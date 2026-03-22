3dc4a26e44502f3ca16ec491f90a142900e2e3ce
gameingame-eng <231358369+gameingame-eng@users.noreply.github.com>
2026-03-22 17:34:34 +0000

    new doctype for int

---
+++ analytic_tracker/analytical/doctype/integer_dash/integer_dash.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Lakshya Dusanapudi && Saivarshan Prabhakaran and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class IntegerDash(Document):
+	pass
